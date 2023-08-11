73c975e7d5e8c56097ae43373be1e33b6e8143e3
ANNIJOICE <annijoice96@gmail.com>
2023-08-11 15:50:13 +0530

    Initial storybook configuration

---
+++ angular-storybook/README.md
@@ -0,0 +1,5 @@
+
+
+## My Reference Link
+
+- https://williamhuey.github.io/posts/nx-angular-storybook-and-cypress-testing/
---
+++ angular-storybook/libs/ui/src/lib/button/button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-storybook-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css'],
+})
+export class ButtonComponent {}
